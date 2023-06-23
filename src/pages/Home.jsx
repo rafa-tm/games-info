a89bd795c2845c44da4f9c1c959493333af8a46b
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-06-23 17:08:52 -0300

    FIX: Erros, filtros, fetch api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import logotipo from "../assets/infoGamesLogo.svg";
 import Button from "../components/Button";
 
+import { MdError } from "react-icons/md";
+import Spinner from "../components/Spinner";
+
 export default function Home() {
   const [filtroNome, setFiltroNome] = useState("");
+  const [filtroGenero, setFiltroGenero] = useState("");
   const [listGames, setListGames] = useState([]);
   const [listGenres, setListGenres] = useState([]);
   const [error, setError] = useState("");
@@ -23,32 +27,31 @@ export default function Home() {
           }
         )
           .then((response) => {
-            console.log(response);
-            return response.json();
-            // if (
-            //   response.status === 500 ||
-            //   response.status === 502 ||
-            //   response.status === 503 ||
-            //   response.status === 504 ||
-            //   response.status === 507 ||
-            //   response.status === 508 ||
-            //   response.status === 509
-            // ) {
-            //   setError(
-            //     "O servidor fahou em responder, tente recarregar a página"
-            //   );
-            // }
-
-            // if (response.status === 200) {
-            //   setError("");
-            //   return response.json();
-            // }
+            if (response.status === 200) {
+              setError("");
+              return response.json();
+            } else if (
+              response.status === 500 ||
+              response.status === 502 ||
+              response.status === 503 ||
+              response.status === 504 ||
+              response.status === 507 ||
+              response.status === 508 ||
+              response.status === 509
+            ) {
+              setError(
+                "O servidor fahou em responder, tente recarregar a página!"
+              );
+            } else {
+              setError(
+                "O servidor não conseguirá responder por agora, tente voltar novamente mais tarde."
+              );
+            }
           })
           .then((data) => {
             setListGames(data);
+            getListGenres(data);
           });
-      } catch (error) {
-        console.log(error);
       } finally {
         setLoading(false);
       }
@@ -57,10 +60,33 @@ export default function Home() {
     fetchGames();
   }, []);
 
+  function getListGenres(data) {
+    const genres = ["Nenhum"];
+
+    if (!data) {
+      return;
+    }
+
+    data.map((game) => {
+      if (!genres.includes(game.genre)) {
+        genres.push(game.genre);
+      }
+    });
+
+    setListGenres(genres);
+  }
+
   function filterGames(tittle) {
     console.log(tittle);
     const filteredGames = listGames.filter((game) => {
       return game.title.toLowerCase().includes(tittle.toLowerCase());
+    });
+    setListGames(filteredGames);
+  }
+
+  function filterGamesByGenre(genre) {
+    const filteredGames = listGames.filter((game) => {
+      return game.genre === genre;
     });
     setListGames(filteredGames);
   }
@@ -77,8 +103,8 @@ export default function Home() {
       </header>
 
       <main className="flex w-full flex-col items-center px-12 py-6">
-        <div className="mb-12 flex w-3/4 items-center justify-center">
-          <div className="flex w-1/2 flex-col items-start gap-4">
+        <div className="mb-12 flex w-[95%] items-center justify-between">
+          <div className="flex w-1/2 flex-col items-center gap-4">
             <label htmlFor="filtroNome" className="font-medium text-slate-50">
               Buscar pelo titulo:
             </label>
@@ -92,7 +118,7 @@ export default function Home() {
                 onChange={(e) => setFiltroNome(e.target.value)}
               />
               <Button
-                type="secondary"
+                type="primary"
                 size="medium"
                 onClick={() => filterGames(filtroNome)}
               >
@@ -106,31 +132,45 @@ export default function Home() {
             <label htmlFor="filtroGenero" className="font-medium text-slate-50">
               Filtrar por gênero:
             </label>
-            <select
-              className="w-full max-w-[20rem] rounded-lg bg-zinc-700 px-4 py-2 font-normal text-zinc-100"
-              type="text"
-              name="filtroGenero"
-              id="filtroGenero"
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-            </select>
-            <Button type="secondary" size="medium">
-              {" "}
-              Buscar{" "}
-            </Button>
+            <div className="flex gap-4">
+              <select
+                className="w-full max-w-[20rem] rounded-lg bg-zinc-700 px-4 py-2 font-normal text-zinc-100"
+                type="text"
+                name="filtroGenero"
+                id="filtroGenero"
+                value={filtroGenero}
+                onChange={(e) => setFiltroGenero(e.target.value)}
+              >
+                {listGenres.map((genre) => {
+                  return (
+                    <option key={genre} value={genre}>
+                      {genre}
+                    </option>
+                  );
+                })}
+              </select>
+              <Button
+                type="primary"
+                size="medium"
+                onClick={() => filterGamesByGenre(filtroGenero)}
+              >
+                {" "}
+                Buscar{" "}
+              </Button>
+            </div>
           </div>
         </div>
 
         {error && (
-          <div className="text-bold m-24 w-3/5 text-center text-xl text-slate-50">
+          <div className="m-24 flex w-3/5 flex-col items-center gap-4 text-center text-2xl font-bold text-slate-50">
+            <MdError size={48} className="text-red-600" />
             <h1>{error}</h1>
           </div>
         )}
 
         {loading && (
-          <div className="text-bold m-24 w-3/5 text-center text-xl text-slate-50">
-            <h1>Carregando</h1>
+          <div className="flex w-full items-center justify-center">
+            <Spinner />
           </div>
         )}
 
